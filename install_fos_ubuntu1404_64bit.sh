cc7ba8e1a056328a5908870e6df4f80027c3c882
cabral-filho <cabralfilhodj@gmail.com>
2019-05-10 05:21:31 -0300

    Update install_fos_ubuntu1404_64bit.sh

--- install_fos_ubuntu1404_64bit.sh
+++ install_fos_ubuntu1404_64bit.sh
@@ -160,7 +160,7 @@ packages_install(){
     tar -xzf /home/fos-streaming/fos-streaming_unpack_x84_64.tar.gz -C /home/fos-streaming/
     rm -rf /home/fos-streaming/fos/www/vendor /home/fos-streaming/fos/www/50x.html > /dev/null 2>&1
     cd /home/fos-streaming/fos/www  > /dev/null 2>&1
-    git clone https://github.com/zgelici/FOS-Streaming-v1.git  > /dev/null 2>&1
+    git clone https://github.com/cabralfilho/FOS-Streaming-v1.git  > /dev/null 2>&1
     cp -R /home/fos-streaming/fos/www/FOS-Streaming-v1/* /home/fos-streaming/fos/www/  > /dev/null 2>&1
 
     echo 'www-data ALL = (root) NOPASSWD: /usr/local/bin/ffmpeg' >> /etc/sudoers  > /dev/null 2>&1
